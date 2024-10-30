1e4192538176485b99c2f60895b27f1327cedc88
kinko6 <rafael.alfalci@gmail.com>
2024-10-30 07:53:52 -0300

    a

---
+++ loja-discos/database/migrations/2024_10_29_154532_create_discos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiscosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discos', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('artista');
+            $table->string('genero');
+            $table->decimal('preco', 8, 2);
+            $table->timestamps();
+        });
+    }
+    
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discos');
+    }
+}
